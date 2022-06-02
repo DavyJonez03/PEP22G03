6364bd80671a94f3f3b973695535888495ca5298
DavyJonez03 <d_golea@yahoo.com>
2022-06-02 19:11:26 +0300

    My first file

--- modul2/part2.py
+++ modul2/part2.py
@@ -1,4 +1,92 @@
-print("\"I\'m\"")
-print("\"\"learning\"\"")
-print("\"\"\"Python\"\"\"")
+# print("\"I\'m\"")
+# print("\"\"learning\"\"")
+# print("\"\"\"Python\"\"\"")
+#
+# print(2 ** 3)
+# print(2 ** 3.)
+# print(2. ** 3)
+# print(2. ** 3.)
+# print(2 * 3)
+# print(2 * 3.)
+# print(2. * 3)
+# print(2. * 3.)
+# print(6 / 3.)
+# print(6. / 3)
+# print(6. / 3.)
+
+# print(6 // 3)
+# print(6 // 3.)
+# print(6. // 3)
+# print(6. // 3.)
+#
+# print(6 // 4)
+# print(6. // 4)
+#
+# print(14 % 4)
+#
+# print(12 % 4.5)
+
+# print(2 + 3 * 5)
+
+# print(9 % 6 % 2)
+
+# print(2 ** 2 ** 3)
+
+# print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+
+# var = 1
+# print(var)
+
+# var = 1
+# account_balance = 1000.0
+# client_name = 'John Doe'
+# print(var, account_balance, client_name)
+# print(var)
+
+# var = "3.8.5"
+# print("Python version: " + var)
+
+# var = 1
+# print(var)
+# var = var + 1
+# print(var)
+
+# var = 100
+# var = 200 + 300
+# # print(var)
+#
+# #INT
+#
+# number = 3
+# print(type(number))
+#
+# # float
+#
+# number = 3.1
+# print(type(number))
+#
+# number = int('3')
+# print('Nr as text', type(number))
+#
+# number = float('3')
+# print('Nr as text', type(number))
+
+# boolean
+
+# True_ = 3
+bool_object = True
+print(type(bool_object))
+print('Address for bool is:',id(bool_object))
+
+
+
+
+
+
+
+
+
+
+
+
 
