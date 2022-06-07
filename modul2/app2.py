0b8544d5c35783aab834df0a4a7f9d49d6493ca8
DavyJonez03 <d_golea@yahoo.com>
2022-06-07 20:11:42 +0300

    My first file

--- modul2/app2.py
+++ modul2/app2.py
@@ -8,16 +8,31 @@
 # print(user)
 
 #exercitiul 6
-# a)
-my_text = ("Hello Python")
-print(my_text[:5], "_", my_text[6:], sep="")
+# # a)
+# my_text = ("Hello Python")
+# print(my_text[:5], "_", my_text[6:], sep="")
+#
+# # b)
+#
+# print(my_text, end=".\n")
+#
+# # c)
+# print(my_text[:5]*4)
 
-# b)
+#exercitiul 7
 
-print(my_text, end=".\n")
+# a = 5.
+# b = 5
+# c = "ana"
+# print(hex(id(a)),type(a))
+# print(hex(id(b)),type(b))
+# print(hex(id(c)),type(c))
 
-# c)
-print(my_text[:5]*4)
+
+
+
+
+
 
 
 
