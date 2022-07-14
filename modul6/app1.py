b70de77039a52b5639b850745c1f36d13b7337c5
DavyJonez03 <you@example.com>
2022-07-14 21:36:48 +0300

    modul 4 - exceptii

--- modul6/app1.py
+++ modul6/app1.py
@@ -5,43 +5,62 @@
 Create application that will ask the user for python input and format the input prompt as needed.
 
 """
-
-
-
 # var1 = ">>>"
-# var2 = "...     "
-#
+# var2 = "..."
+# var3 = "    "
+# counter = 0
+# in_try = False
 # while True:
-#     imp = input(f"{var1} ")
-#     if imp == "try:":
-#         imp2 = input(f"{var2}")
-#         print(imp2)
-#     else:
-#         exit()
+#     x = input(f"{var1}")
+#     try:
+#         if x[-1] == ":":
+#             counter += 1
+#             var1 = "..." + counter * var3
+#         if x.endswith("try:"):
+#             in_try = True
+#
+#         if x.endswith("finally:"):
+#             in_try = False
+#     except IndexError:
+#         if counter == 0 and not in_try:
+#             quit()
+#         counter -= 1
+#         var1 = "..." + counter * var3
 
 
-var1 = ">>>"
-var2 = "..."
-var3 = "    "
-counter = 0
-in_try = False
-while True:
-    x = input(f"{var1}")
-    try:
-        if x[-1] == ":":
-            counter += 1
-            var1 = "..." + counter * var3
-        if x.endswith("try:"):
-            in_try = True
+"""
 
-        if x.endswith("finally:"):
-            in_try = False
-    except IndexError:
-        if counter == 0 and not in_try:
-            quit()
-        counter -= 1
-        var1 = "..." + counter * var3
+Create list of prime number up to specified limit using dedicated function
 
+"""
+
+
+import random
+
+def generate_primes(limit: int):
+    result=[]
+    for i in range(1, limit + 1):
+        for j in range(2, i//2+2):
+            if i % j == 0:
+                break
+        else:
+            result.append(i)
+    print(result)
+
+generate_primes(100)
+"""
+
+Select 2(distinct) random numbers from prime numbers generated above.
+
+"""
+
+def select_primes(in_list: list):
+    i = random.choice(in_list)
+    in_list.remove(i[0])
+    j = random.choice(in_list)
+    return i[0], j[0]
+
+print(select_primes(generate_primes(100)))
 
 
 
