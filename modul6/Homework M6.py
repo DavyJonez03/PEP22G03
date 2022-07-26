d7b9fa3a78fdb644aabfe34ab2e8c655837369a9
DavyJonez03 <you@example.com>
2022-07-26 16:04:05 +0300

    modul 4 - exceptii

---
+++ modul6/Homework M6.py
@@ -0,0 +1,7 @@
+# Create a function that:
+#
+# takes in 2 primes numbers (x, y) where x < y
+# generates a random number z in the interval 2 to y
+# returns (x^z)%y x = pow(self.base, self.__local_secret) % self.prime
+# self. = pow(secret, self.local_secret) % self.prime + 129
+
