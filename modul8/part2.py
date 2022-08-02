7b816b7b5b940f2a5323b8b0815c3d41f746144f
DavyJonez03 <you@example.com>
2022-08-02 20:28:26 +0300

    modul 4 - exceptii

--- modul8/part2.py
+++ modul8/part2.py
@@ -1,13 +1,48 @@
 # Iterator
 
-class MyIterator():
-    def __init__(self, number:int):
+class MyNumber(int):
+
+    def __init__(self, number):
         self.number = number
 
     def __iter__(self):
+        return MyIterator(self.number)
+
+
+class MyIterator():
+
+    def __init__(self, number: int):
+        self.number = number
+
+    def __iter__(self):
+        return self
 
     def __next__(self):
-        pass
+        if not self.number:
+            raise StopIteration
+        self.number -= 1
+        return self.number
 
-for i in MyIterator(3):
+my_number = MyNumber(15)
+
+print('result',  my_number + 3)
+
+for i in my_number:
     print(i)
+
+
+# my_str = "abc"
+# print(my_str.__iter__())
+# print(my_str.__iter__().__iter__())
+
+# class MyShop:
+#
+#     def __int__(self):
+#         self.products = {}
+#
+#     def add_products(self):
+#         self.products['x'] = 'y'
+#
+# shop = MyShop()
+# for product in shop:
+#     print(product)
