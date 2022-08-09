b261779b4c1b46ddee273bf11e2171886020594b
DavyJonez03 <you@example.com>
2022-08-09 19:40:54 +0300

    modul 4 - exceptii

--- modul6/app1.py
+++ modul6/app1.py
@@ -5,27 +5,27 @@
 Create application that will ask the user for python input and format the input prompt as needed.
 
 """
-# var1 = ">>>"
-# var2 = "..."
-# var3 = "    "
-# counter = 0
-# in_try = False
-# while True:
-#     x = input(f"{var1}")
-#     try:
-#         if x[-1] == ":":
-#             counter += 1
-#             var1 = "..." + counter * var3
-#         if x.endswith("try:"):
-#             in_try = True
-#
-#         if x.endswith("finally:"):
-#             in_try = False
-#     except IndexError:
-#         if counter == 0 and not in_try:
-#             quit()
-#         counter -= 1
-#         var1 = "..." + counter * var3
+var1 = ">>>"
+var2 = "..."
+var3 = "    "
+counter = 0
+in_try = False
+while True:
+    x = input(f"{var1}")
+    try:
+        if x[-1] == ":":
+            counter += 1
+            var1 = "..." + counter * var3
+        if x.endswith("try:"):
+            in_try = True
+
+        if x.endswith("finally:"):
+            in_try = False
+    except IndexError:
+        if counter == 0 and not in_try:
+            quit()
+        counter -= 1
+        var1 = "..." + counter * var3
 
 
 """
